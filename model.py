d42f3c4f6d7f0b6fb24be9a90e66d0e0d0a7ed2b
Karthic Rao <karthic@minio.io>
2017-06-06 18:37:41 +0530

    Adding simple neural net based model
    - Built a simple regression neural network with one output node trained
      to predict the steering measurement given the image.

--- model.py
+++ model.py
@@ -20,13 +20,30 @@ steeringMeasurements = []
 for line in lines:
 	imgPath = line[0]
 	img = cv2.imread(imgPath)
-	cameraImages.append(image)
+	cameraImages.append(img)
 
 	measurement = float(line[3])
-	steeringMeasurements.append(measurements)
+	steeringMeasurements.append(measurement)
 
 # convert the images and measurements to numpy array.
 xTrain = np.array(cameraImages)
 yTrain = np.array(steeringMeasurements)
 
+# Using keras to build and train the model.
+from keras.models import Sequential
+from keras.layers import Flatten, Dense 
+# building a sequential regression network (not an classification network).
+# the model will be trained on the image and using streeing measurements as output data.
+# single output node will predict the streeing measurement.
+model = Sequential()
+# the input image is of dimention 160 x 320 x 3
+model.add(Flatten(input_shape=(160,320,3)))
+# the network has just one output node.
+model.add(Dense(1))
+# Defining mean square error to be the loss functions and using Adam optimizer.
+model.compile(loss='mse', optimizer='adam')
+
+model.fit(xTrain, yTrain, validation_split=0.2, shuffle=True)
+
+model.save('model.h5')
 
